566f73c05d6b024694657c917b3931cc9c3586ba
Miguel Bucarito <miguelebucarito2025@gmail.com>
2026-07-29 18:02:18 -0400

    creacion de los bocetos de las primeras interfaces

--- public/plantilla.php
+++ public/plantilla.php
@@ -1,3 +1,10 @@
+<?php
+
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Pragma: no-cache");
+header("Expires: 0");
+
+?>
 <!doctype html>
 <html lang="es">
 
@@ -5,15 +12,17 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>SVPMPH-oficial V=2</title>
-  <link rel="stylesheet" href="public/css/style.css" />
+  <link rel="stylesheet" href="public/css/style.css?v=<?php echo date('dd-yy-ww') ?>" />
   <script src='public/js/alert.js' defer></script>
 </head>
 
 <body>
 
-  <?php
-  include_once 'public/views/alert.php';
-  ?>
+
+
+
+
+  <?php include_once 'public/views/alert.php'; ?>
 </body>
 
 </html>
